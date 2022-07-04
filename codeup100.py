df26f3a4ed565a9c669bc56bb94f6f652bf27b4f
DESKTOP-IVKH1A7\kswus <ksw011572@gmail.com>
2022-07-04 16:29:42 +0900

    first push

---
+++ codeup100.py
@@ -0,0 +1,33 @@
+#codeup
+# 6001
+print("Hello")
+
+# 6002
+print("Hello World")
+
+# 6003
+print("Hello\nWorld")
+
+# 6004
+print("\'Hello\'")
+
+# 6005
+print("\"Hello World\"")
+
+# 6006
+print("\"!@#$%^&*()\'")
+
+# 6007
+print("\"C:\\Download\\\'hello\'.py\"")
+
+# 6008
+print("\"Hello\\nWorld\"")
+
+# 6009
+c = input()
+print(c)
+
+# 6010
+n = input()
+n = int(n)
+print(n)
